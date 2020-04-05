512e6839fd6a04473c230f80d5d44aef7da9e77c
ValeedAnjum <allinone644@gmail.com>
2020-04-05 12:00:29 +0500

    Response from server

--- app.js
+++ app.js
@@ -2,12 +2,23 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+//
+app.use(function(req,res,next) {
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','GET');
+    next();
+})
+//
 app.get('/',(req,res) => {
     res.send("Hello from home");
 })
 
 app.get('/about',(req,res) => {
     res.send("Hello from about");
+})
+
+app.get('/home',(req,res) => {
+    res.send({msg:'I am response from server'});
 })
 
 app.listen(port,() => {
